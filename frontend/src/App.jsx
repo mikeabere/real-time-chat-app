2ad0a8ca11aac1ca95915977f4d480912fdd4d73
mike mwanzi <mikeabere@gmail.com>
2025-05-30 22:07:29 +0300

    import messageinput

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import ChatPage from "./pages/ChatPage";
 import ProtectedRoute from "./components/ProtectedRoute";
+import MessageInput from "./components/MessageInput";
 
 const App = () => (
   <Routes>
     <Route path="/register" element={<Register />} />
     <Route path="/login" element={<Login />} />
     <Route path="/chatpage" element={<ChatPage />} />
+    <Route path="/messageinput" element={<MessageInput />} />
     <Route
       path="/chat"
       element={
